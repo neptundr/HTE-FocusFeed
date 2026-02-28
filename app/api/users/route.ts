8776f8c09795a85a873e8c6436e04c25d76b163f
neptundr <neptundr@mail.ru>
2026-03-01 07:51:36 +0800

    Deployment Fixes 2

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const { prisma } = await import("@/lib/prisma");
   const users = await prisma.user.findMany({
